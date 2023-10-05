a56a4f91fd158dc6df5708ce3e2cb7e1630c0e80
Gkrab228 <118478301+Gkrab228@users.noreply.github.com>
2023-10-05 19:47:02 +0200

    Add files via upload

---
+++ Java Script 01/scripts.js
@@ -0,0 +1,6 @@
+var button = document.querySelector("#changeColorText");
+var text = document.querySelector("#mainText");
+button.onclick = function() {
+    text.classList.toggle("textGreen");
+    
+};
